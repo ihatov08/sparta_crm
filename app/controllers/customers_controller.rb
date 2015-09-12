63e68340b5f144cd697f23e550e8bf9d47e6644d
吉田智哉 <yoshidatomoya@tom.local>
2015-09-12 12:47:31 +0900

    create編集

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,17 +7,45 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      # 成功時
+    redirect_to @customer
+  else
+    # 失敗
+    render :new
+  end
+
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+    redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email,
+      )
+  end
+
 end
